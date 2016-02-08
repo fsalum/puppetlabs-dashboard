92b04c4d1b3215e4ee52eb062e0b76526f335f99
Radu Tizu <radu.tizu@gmail.com>
2016-02-08 14:20:22 +0000

    Removed duplicate declaration of $dashboard_config

--- manifests/init.pp
+++ manifests/init.pp
@@ -108,7 +108,6 @@ class dashboard (
   $passenger                = $dashboard::params::passenger,
   $mysql_package_provider   = $dashboard::params::mysql_package_provider,
   $ruby_mysql_package       = $dashboard::params::ruby_mysql_package,
-  $dashboard_config         = $dashboard::params::dashboard_config,
   $dashboard_root           = $dashboard::params::dashboard_root,
   $rack_version             = $dashboard::params::rack_version,
   $dashboard_workers_start  = $dashboard::params::dashboard_workers_start,
